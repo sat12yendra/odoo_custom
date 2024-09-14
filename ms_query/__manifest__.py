e01bb9925b0e855e8bbcbeafc31de314bbe7b9cf
Your Name <you@example.com>
2024-09-14 13:17:29 +0530

    Add query module

---
+++ ms_query/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": "Execute Query",
+    "summary": """
+        Execute query without open postgres
+    """,
+    "description": """
+    """,
+    "author": "Miftahussalam",
+    "website": "https://blog.miftahussalam.com/",
+    "category": "Extra Tools",
+    "version": "17.0.1.0.0",
+    "depends": [
+        "base",
+        "mail",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/ms_query_view.xml",
+    ],
+    "demo": [
+
+    ],
+    "images": [
+        "static/description/images/main_screenshot.png",
+    ],
+    "license": "LGPL-3",
+}
